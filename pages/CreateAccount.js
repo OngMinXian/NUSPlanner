2033854158ee1ac4e4a593c327245c6d8f8d8228
OngMinXian <95210624+OngMinXian@users.noreply.github.com>
2022-05-24 03:12:27 +0800

    Delete pages directory

--- pages/CreateAccount.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const CreateAccount = () => {
-  return (
-    <div>Create account page</div>
-  )
-}
-
-export default CreateAccount
--- pages/SignIn.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function SignIn() {
-  return (
-    <div>Sign in page</div>
-  )
-}
-
-export default SignIn
