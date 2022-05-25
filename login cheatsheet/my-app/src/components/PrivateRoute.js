32e2d5b5688d130f8a981b3a82d774d3adf386e5
wzs02 <shanicesnglol@gmail.com>
2022-05-25 16:25:00 +0800

    login page

---
+++ login cheatsheet/my-app/src/components/PrivateRoute.js
@@ -0,0 +1,8 @@
+import {  Navigate } from "react-router-dom";
+import { useAuth } from "../components/contexts/AuthContext"
+
+export default function PrivateRoute({ children }) {
+    const { currentUser } = useAuth();
+  
+    return currentUser ? children : <Navigate to="/login" />;
+  }
