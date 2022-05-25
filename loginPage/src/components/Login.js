6a5ce3ea8f1b25943f7550ff0c32e5cadc24c8b0
wzs02 <shanicesnglol@gmail.com>
2022-05-25 20:58:05 +0800

    layout

--- loginPage/src/components/Login.js
+++ loginPage/src/components/Login.js
@@ -1,9 +1,11 @@
-import React, {useRef,useState} from 'react'
+import React, { useRef, useState } from 'react'
 import { Form, Button, Card, Alert } from 'react-bootstrap'
 import { useAuth } from "../components/contexts/AuthContext"
 import { Link, useNavigate } from "react-router-dom"
 import Icon from "../images/icon.png"
 import Background from "../images/background1.jpg"
+import "./CSS/style.css"
+
 
 export default function Login() {
     const emailRef = useRef()
@@ -26,45 +28,50 @@ export default function Login() {
         }
         setLoading(false)
     }
-  return (
-    <> 
-              <img src={Icon} />
-              <h1>NUSPlanner</h1>
-              <h2> Log In </h2>
-              {error && <Alert variant="danger">{error}</Alert>}
-              <form onSubmit = {handleSubmit}>
+    return (
+        <>
+            <div className ="container-1">
+                <div>
+                    <img src={Icon} height="247" width="170" />
+                    <h1>NUSPlanner</h1>
+                    <h2> Log In </h2>
+                    {error && <Alert variant="danger">{error}</Alert>}
+                    <form onSubmit={handleSubmit}>
 
-                  
-                      <label>Email:</label>
-                      <input 
-                        type="text"
-                        required="required"
-                        ref={emailRef}
-                        ></input>
-                  
+                        <form>
+                            <label>Email:</label>
+                            <input
+                                type="text"
+                                required="required"
+                                ref={emailRef}
+                            ></input>
+                        </form>
 
-                  
-                      <label>Password:</label>
-                      <input 
-                        type="text"
-                        required="required"
-                        ref={passwordRef}
-                        ></input>
-                  
+                        <form>
+                            <label>Password:</label>
+                            <input
+                                type="text"
+                                required="required"
+                                ref={passwordRef}
+                            ></input>
+                        </form>
 
-                  <button disabled = {loading}  type = "submit">Log in</button>
-              </form>
-              <div>
-                  <Link to = "/forgot-password">Forgot Password?</Link>
-              </div>
-
-      <img src={Background} />
-      <div> 
-          <h2>Haven't created an account yet?</h2>
-          <h3>Sign up now!</h3>
-          <Link to = '/signup'> Sign Up </Link> 
-      </div>
-    </>
-  )
+                        <button disabled={loading} type="submit">Log in</button>
+                    </form>
+                    <div>
+                        <Link to="/forgot-password">Forgot Password?</Link>
+                    </div>
+                </div>
+                <div className = "logo">
+                    <img src={Background} height = "1023" width = "509" />
+                    <div>
+                        <h2>Haven't created an account yet?</h2>
+                        <h3>Sign up now!</h3>
+                        <Link to='/signup'> Sign Up </Link>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
 }
 
