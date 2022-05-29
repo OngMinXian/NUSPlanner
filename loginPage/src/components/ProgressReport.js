f2f2fe215c3e6628a565fa7c0c7d27c198afcf78
OngMinXian <ongminxian999@gmail.com>
2022-05-29 11:36:47 +0800

    Added sidebar to progress report

--- loginPage/src/components/ProgressReport.js
+++ loginPage/src/components/ProgressReport.js
@@ -1,14 +1,11 @@
 import React, { useState, useEffect } from 'react'
-import SideBar from "./Sidebar"
-import { addDoc, collection, getDocs, deleteDoc, doc, where, query, orderBy } from "firebase/firestore";
-import { db, auth } from "../firebase";
-import "./CSS/task.css"
-import { BsFillTrashFill } from 'react-icons/bs';
-import Popup from "./Popup"
+import SideBar from './Sidebar.js';
 
 function ProgressReport() {
     return (
-      <></>
+      <>
+      <SideBar></SideBar>
+      </>
     )
   }
 
