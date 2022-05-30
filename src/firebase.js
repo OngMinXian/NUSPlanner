a51b4b7680ff2395326275521aa8527d83e705b6
OngMinXian <ongminxian999@gmail.com>
2022-05-31 00:57:22 +0800

    Initial commit

--- src/firebase.js
+++ src/firebase.js
@@ -1,32 +1,27 @@
 import firebase from 'firebase/compat/app'
-import "firebase/compat/auth" //DO NOT USE firebase/auth
+import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
+import { getAuth } from "firebase/auth"
 
-const app = firebase.initializeApp({
-apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+const firebaseConfig = {
+    apiKey: "AIzaSyBeJmFpYb7eufPfTnLgDNheYeRpq3utSGY",
+    authDomain: "auth-development-71ec2.firebaseapp.com",
+    databaseURL: "https://auth-development-71ec2-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "auth-development-71ec2",
+    storageBucket: "auth-development-71ec2.appspot.com",
+    messagingSenderId: "1022464777549",
+    appId: "1:1022464777549:web:50adc0a5df3589f3386b04",
+    measurementId: "G-K4HSK43RRS"
+  };
+  
+const app = initializeApp(firebaseConfig);
 
-authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-
-projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-
-storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-
-appId: process.env.REACT_APP_FIREBASE_APP_ID,
-
-measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
-})
-
-export const auth = app.auth()
+export const auth = getAuth(app);
 export const db = getFirestore(app);
 export const storage = getStorage(app);
 
 export default app
-
-
-
-
 
 
 
